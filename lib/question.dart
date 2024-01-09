2f9fa635bb77e3956927af8afb09ab01e79bf7a4
Shuvam Paul <shuvam.paul2005@gmail.com>
2024-01-09 21:46:03 +0530

    first commit

---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question(this.questionText, this.questionAnswer);
+}
+
+// style: const ButtonStyle(
+// backgroundColor: MaterialStatePropertyAll(Colors.green),
+// ),
